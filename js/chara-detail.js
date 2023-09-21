8c10e4361d3d126a050f5d6bdc5dccc19d0fdaf2
PuppiizSunniiz <general_kiroro@hotmail.com>
2023-09-21 14:19:56 +0700

    Merge all Puppiz

--- js/chara-detail.js
+++ js/chara-detail.js
@@ -94,6 +94,7 @@
     var spinewidget
     var spinewidgetcg
     var curropname
+    var defaulttoken
     var globaltoken
     var globalelite = 0
     var globallevel =[1,1,1]
@@ -217,7 +218,7 @@
                     LoadAnimation()
                 }
                 if(!spinewidgettoken){
-                    if(opdataFull.tokenKey){
+                    if(globaltoken){
                         LoadAnimationToken()
                     }
                 }
@@ -312,7 +313,7 @@
                 LoadAnimation()
             }
             if(spinewidgettoken){
-                if(opdataFull.tokenKey){
+                if(globaltoken){
                     LoadAnimationToken()
                 }
             }
@@ -1358,8 +1359,17 @@
             // console.log(charalist.join("\n"))
             //
 
+            //get globaltoken early for tokenchibi (L:1465-)
+            defaulttoken = null
+            $.each(opdataFull.talents,(_,t)=>{
+                if (!defaulttoken&&t.candidates[0].tokenKey){
+                    defaulttoken=t.candidates[0].tokenKey
+                    return false
+                }
+            })
+            globaltoken=defaulttoken?defaulttoken:opdataFull.skills[0]?opdataFull.skills[0].overrideTokenKey:null
 
-            tokenname = opdataFull.tokenKey
+            tokenname = globaltoken
             currskin =opcode
             currVoiceID = opdataFull.id
 
@@ -1986,8 +1996,7 @@
                     var skillIcon = skillData.iconId;
                 }
 
-                var skilltoken = opdataFull.skills[i].overrideTokenKey
-                if(skilltoken== null) skilltoken = opdataFull.tokenKey
+                var skilltoken = opdataFull.skills[i].overrideTokenKey?opdataFull.skills[i].overrideTokenKey:defaulttoken
 
                 var tabItem = $(`
                 <li class='nav-item'>
@@ -2023,7 +2032,6 @@
             });
             //TOKEN SOON ??
             $("#token-contents").html("")
-            globaltoken = opdataFull.tokenKey
             globalelite = 0
             UpdateTokenContent()
             //EQUIP CHECK
@@ -2796,10 +2804,14 @@
         ChangeSkillAnim(i,skillLength,skilltoken)
         globalskill = i
         UpdateTokenContent()
+        if(loadchibi && !defaulttoken && skilltoken.includes("token_")){
+            LoadAnimationToken()
+        }
     }
 
     function UpdateTokenContent(){
         if(!globaltoken){
+            $("#token-contents").html("");
             return
         }
         var tokenfulldata = db.chars[globaltoken]
@@ -3634,7 +3646,7 @@
                     filteredFX.push({name:fxname[fxname.length-1],dataname:element.name,dir:fxdir,type:"3"})
                 });
             }
-            if(element.name.includes(opdataFull.tokenKey)){
+            if(element.name.includes(globaltoken)){
                 // console.log(element.name)
                 element.sounds.forEach(soundfx => {
                     var fxname = soundfx.asset.split("/")
@@ -5595,7 +5607,7 @@
     }
 
 
-    function LoadAnimationToken(tokenkey=opdataFull.tokenKey){
+    function LoadAnimationToken(tokenkey=globaltoken){
         // console.log(spinewidgettoken)
         // console.log(opdataFull)
         // var tokenName =
