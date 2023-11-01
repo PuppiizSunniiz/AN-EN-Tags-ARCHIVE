84af844d56f7cfdb9ad0d882309a7b47f1c7d8d4
PuppiizSunniiz <general_kiroro@hotmail.com>
2023-11-01 18:12:18 +0700

    4.5 Anni forgor loopshooter trait

---
+++ pyCombination.py
@@ -0,0 +1,25 @@
+import json
+
+###
+# json
+###
+
+jsonlist = json.loads(open("json/gamedata/zh_CN/gamedata/excel/activity_table.json").read())
+
+formulas = jsonlist["activity"]["TYPE_ACT29SIDE"]["act29side"]["formDataMap"]
+
+notes = jsonlist["activity"]["TYPE_ACT29SIDE"]["act29side"]["fragDataMap"]
+
+parse = {}
+for note in notes.keys():
+    parse[note]=notes[note]["fragName"]
+
+mood={"乐":"Happy","怒":"Fury","哀":"Sorrow","惧":"Fear"}
+
+color={"乐":"🟨","怒":"🟥","哀":"🟦","惧":"🟩"}
+
+for i in formulas.keys():
+    #print(formulas[i]["formDesc"],[parse[x] for x in formulas[i]["fragIdList"]])
+    #print([mood[parse[x]] for x in formulas[i]["fragIdList"]])
+    print(formulas[i]["formDesc"],[parse[x] for x in formulas[i]["fragIdList"]],[color[parse[x]] for x in formulas[i]["fragIdList"]])
+    
